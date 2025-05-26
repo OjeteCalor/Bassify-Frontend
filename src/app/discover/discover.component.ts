31feda8fe2a10a6558fa26d8195befac224b5307
Guillermo Ramos Fernandez metrica <guillermo.ramos.fer@gmail.com>
2025-05-26 16:39:28 +0200

    change page Discover

--- src/app/discover/discover.component.ts
+++ src/app/discover/discover.component.ts
@@ -5,11 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './discover.component.html',
   styleUrls: ['./discover.component.css']
 })
-export class DiscoverComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class DiscoverComponent {
+  movimiento: string = 'translateX(0)';
+  aceptCard(){
+    this.movimiento = 'translateX(100px)';
   }
 
 }
