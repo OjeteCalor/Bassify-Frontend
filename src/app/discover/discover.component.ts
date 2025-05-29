90818572664040af63a95ac0064c1613ae957ff3
Dani Vals Metrica <daniel.vals.simon@gmail.com>
2025-05-29 09:25:30 +0200

    FEAT: leer canciones y parsear a Track desde archivo placeholder

--- src/app/discover/discover.component.ts
+++ src/app/discover/discover.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Track } from '../Class/Track';
 
 @Component({
-  selector: 'app-discover',
-  templateUrl: './discover.component.html',
-  styleUrls: ['./discover.component.css']
+	selector: 'app-discover',
+	templateUrl: './discover.component.html',
+	styleUrls: ['./discover.component.css']
 })
-export class DiscoverComponent {
+export class DiscoverComponent implements OnInit {
 
+	tracks: Track[] = [];
+
+	ngOnInit(): void {
+		fetch('/canciones.json')
+			.then(response => response.json())
+			.then(data => {
+				for (const trackData of data['tracks']) {
+					const track = Track.parseJSON(trackData);
+					this.tracks.push(track);
+				}
+			})
+			.catch(error => console.error('Error al leer el archivo:', error))
+		;
+	}
 
 }
