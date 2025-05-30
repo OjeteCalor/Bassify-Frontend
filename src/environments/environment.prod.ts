2c8c96012b62c547d1c73883f9391a9d7d78b9e2
Dani Vals Metrica <daniel.vals.simon@gmail.com>
2025-05-30 10:25:29 +0200

    FIX: redirect Spotify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   spotifyClientId: '5c39287da05b4fff8d4aa4dc6856a689',
-  spotifyRedirectUri: 'http://127.0.0.1:4200/callback',
+  spotifyRedirectUri: 'http://127.0.0.1:4200/acc/callback',
   spotifyScope: 'user-read-email user-read-private',
 //ng serve --host 127.0.0.1 --port 4200 para que funcione el athentification
 };
