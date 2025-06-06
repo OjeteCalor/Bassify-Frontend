aaad5983cd2eaa2829cb281cd72fbfbddfb9535c
Dani Vals Metrica <daniel.vals.simon@gmail.com>
2025-06-06 13:53:32 +0200

    FEAT: no te permite entrar en paginas sin iniciar sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,19 +10,38 @@ import { CallbackComponent } from './accountManagement/callback/callback.compone
 import { LogoutComponent } from './accountManagement/logout/logout.component';
 import { FirstPreferencesComponent } from './accountManagement/first-preferences/first-preferences.component';
 import { DiscoverRandomComponent } from './discover-random/discover-random.component';
+import { CanActivate, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+	providedIn: 'root'
+})
+class AuthGuard implements CanActivate {
+	constructor(private router: Router) {}
+
+	canActivate(): boolean {
+		if (sessionStorage.getItem('spotifyId')) {
+			return true;
+		}
+		this.router.navigate(['/acc/login']);
+		return false;
+	}
+}
 
 const routes: Routes = [
-	{ path: 'home', component: HomeComponent },
+	{ path: 'home', component: HomeComponent},
+
 	{ path: 'acc', component: AccountManagementComponent, children:[
 		{path: 'callback', component: CallbackComponent },
 		{path: 'first-preferences', component: FirstPreferencesComponent},
 		{path: 'login', component: LoginComponent},
 		{path: 'logout', component: LogoutComponent}
 	]},
-	{ path: 'settings', component: SettingsComponent },
-	{ path: 'discover', component: DiscoverComponent },
-	{ path: 'discoverRandom', component: DiscoverRandomComponent },
-	{ path: 'about-us', component: AboutUsComponent },
+	
+	// { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
+	{ path: 'discover', component: DiscoverComponent, canActivate: [AuthGuard] },
+	{ path: 'discoverRandom', component: DiscoverRandomComponent, canActivate: [AuthGuard] },
+	// { path: 'about-us', component: AboutUsComponent },
 
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 	{ path: '**', redirectTo: 'home' }
